0b0225b3a4ba08f4fd56f85158a91ea8c3bea02a
MichaelSailer <sailer.michi@gmx.at>
2020-03-09 10:33:46 +0100

    Datenbank-Systementwicklung

---
+++ Datenbankorientierte Systementwicklung/Aufgabe_1/js/app.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("nav").style.width = "200px";
+  }
+  
+  function closeNav() {
+    document.getElementById("nav").style.width = "0";
+  }
---
+++ Datenbankorientierte Systementwicklung/Aufgabe_1/js/slide.js
@@ -0,0 +1,26 @@
+var index = 1;
+
+function goRight(){
+    let img_old = document.getElementById("pic");
+
+    index ++;
+    if(index > 4){index = 1}
+
+    switch(index){
+        case 1: 
+            img_old.src = "../img/1.jpg";
+        break;
+        case 2: 
+            img_old.src = "../img/2.png";
+        break;
+        case 3: 
+            img_old.src = "../img/3.jpg";
+        break;
+        case 4: 
+            img_old.src = "../img/4.jpg";
+        break;
+    }
+
+
+
+}
